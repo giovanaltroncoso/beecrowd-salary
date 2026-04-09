6776bcfd9045bef29c0f56a18cee691e23e07bfc
Giovana <giovanaltc@outlook.com>
2026-04-09 13:39:45 -0300

    Versão em português

--- pt-br/Main.java
+++ pt-br/Main.java
@@ -8,16 +8,16 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int idFuncionario = sc.nextInt(); // Lê o número do funcionário
-        int horasTrabalhadas = sc.nextInt(); // Lê a quantidade de horas
-        double valorHora = sc.nextDouble(); // Lê o valor recebido por hora
+        int idFuncionario = sc.nextInt(); //Lê o número do funcionário
+        int horasTrabalhadas = sc.nextInt(); //Lê a quantidade de horas
+        double valorHora = sc.nextDouble(); //Lê o valor recebido por hora
         
         double salario = horasTrabalhadas * valorHora; //Cálculo do salário total
 
         System.out.printf("""    
                 NUMBER = %d
                 SALARY = U$ %.2f
-                """, idFuncionario, salario); // %d é usado para números inteiros e %.2f para decimais com 2 casas
+                """, idFuncionario, salario); //%d é usado para números inteiros e %.2f para decimais com 2 casas
 
         sc.close();
     }
